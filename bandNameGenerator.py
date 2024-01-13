348eb30491ba39ca359fdea6ee945dd3344e2359
TECH FOR PEOPLE <67988759+esundara@users.noreply.github.com>
2024-01-13 15:34:36 -0500

    String Manipulation
    Covered string manipulation and did a project associated with it.

---
+++ bandNameGenerator.py
@@ -0,0 +1,5 @@
+#This program will combine an adjective and animal you have provided
+adjective = input("Please enter an adjective: ")
+animal = input("Please enter the name of an animal:")
+bandName = adjective + " " + animal
+print("The name of your band is: " + bandName + ".\nVery unique!")
---
+++ stringManipulationNotes.py
@@ -0,0 +1,33 @@
+#to have quotes in a string, use single quotes to surround the statement itself and use double quotes for the overall statement
+#example:
+print("Bob said 'hi'")
+
+
+#to have quotes in a string, use single quotes to surround the statement itself and use double quotes for the overall statement
+#example:
+print('Jeff said "hello"')
+#to do print multiple lines using one print statement, use \n
+#example:
+print("Hello World\nHello World\nHello World")
+
+#To get input from the user, use the input() function
+#example:
+input("Example prompt for the user")
+
+
+#the line is replaced by what ever value was inputted, so you could even do this:
+#input() will first get user input in the console
+#Then. print() will print the word hello and the user input 
+print("Hello" + input("what is your name?"))
+
+#to find the number of characters in a string, use the len() function 
+#example: 
+exampleName = "bob"
+print(len(exampleName))
+
+#when you are creating a variable type in python, you do not have to mention the variable type
+#for example:
+phone = 123
+name = "tom"
+
+#variable naming tip: cannot start a variable name with a number (but you could write out the numbers name)
