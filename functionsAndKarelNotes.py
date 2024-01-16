a8005e3dc8147c646c78b7f4de3288761a76b086
Jai Sundaram <je.sundaram@gmail.com>
2024-01-16 12:34:59 -0500

    Create functionsAndKarelNotes.py

---
+++ functionsAndKarelNotes.py
@@ -0,0 +1,11 @@
+#to create a functio, to do the follow: def myFuntiion():
+#example:
+def simple():
+    print("hello")
+    print("hi")
+#to call the function: myFunction
+simple()
+
+#an indentation is 4 spaces
+
+#not - negatation of the condition. Basically the opposite
